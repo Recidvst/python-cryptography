0b5518aeac42ed81eeb208c86f72c1609251f897
Chris Snowden <cdsnowden@gmail.com>
2019-03-27 23:19:06 +0000

    Update stringToImage.py

--- stringToImage.py
+++ stringToImage.py
@@ -7,10 +7,13 @@ if len(sys.argv) > 1:
 
     try:
         with open(openFile, 'rb') as imageFile:
-            data=base64.b64decode(imageFile.read())
-            fi = open(writeFile, "wb")
+            data = base64.b64decode(imageFile.read()) # decode from base64
+            fi = open(writeFile, "wb") # open end image file to write to
             fi.write(data)
             fi.close()
 
     except FileNotFoundError:
         sys.exit('Error: target file not found or write to image file failed')
+
+else : 
+    sys.exit('Please provide a file containing a base64 encoded image as an argument')
